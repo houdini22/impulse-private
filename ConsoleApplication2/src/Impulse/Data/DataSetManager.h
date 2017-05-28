c62d7f34b9e3c8165d97ac0f8b622ef5ac8f70f5
hud <baniczek@gmail.com>
2017-05-28 03:43:35 +0200

    output change

--- ConsoleApplication2/src/Impulse/Data/DataSetManager.h
+++ ConsoleApplication2/src/Impulse/Data/DataSetManager.h
@@ -14,14 +14,16 @@ public:
 
         for (int i = 0; i < input.rows(); i++) {
 
+			/*
 			Eigen::VectorXd newOutput(10);
             double realValue = output(i, 0);
             for (int j = 1; j <= 10; j++) {
                 newOutput(j - 1) = j == realValue ? 1.0 : 0.0;
             }
+			*/
 
 			Eigen::VectorXd newInput(input.row(i));
-			// Eigen::VectorXd newOutput(output.row(i));
+			Eigen::VectorXd newOutput(output.row(i));
 		
             MapSample dataSample = MapSample();
 			dataSample["input"] = newInput;
